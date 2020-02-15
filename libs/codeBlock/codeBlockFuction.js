fdf9efab864c4d2d5d4a5eaee435c9ae0f498f24
tuyilin <tuyilin2004@qq.com>
2020-02-15 15:33:48 +0800

    Site updated: 2020-02-15 15:33:23

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,3 @@
-// 代码块功能依赖
-
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+// build time:Sat Feb 15 2020 15:28:13 GMT+0800 (GMT+08:00)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
